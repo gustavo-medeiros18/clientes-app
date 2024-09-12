2c00120ff89a1ed2f3baba69c08055b43c5367e7
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-12 10:17:30 -0300

    Imports FormsModule in ClientesModule.

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { ClientesRoutingModule } from './clientes-routing.module';
 import { ClientesFormComponent } from './clientes-form/clientes-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ClientesFormComponent],
   imports: [
     CommonModule,
-    ClientesRoutingModule
+    ClientesRoutingModule,
+    FormsModule
   ],
   exports: [ClientesFormComponent]
 })
