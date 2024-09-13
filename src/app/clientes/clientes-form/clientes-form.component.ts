aa8a7b596753773d21dec380d678eff745116508
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-13 15:24:21 -0300

    Sets errors as null and cliente as response if request is successful in ClientesFormComponent.onSubmit() method.

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -23,13 +23,16 @@ export class ClientesFormComponent implements OnInit {
      * The subscribe method is used to execute the Observable returned by the salvar method.
      * It is necessary to call it here to actually send the HTTP POST request and handle the
      * response or any errors that may occur.
-     * 
+     *
      * First callback function is executed when the request is successful. The second callback
      * function is executed when an error occurs.
      */
     this.service.salvar(this.cliente).subscribe(
       (response) => {
-      this.success = true;
+        this.success = true;
+        this.errors = null;
+
+        this.cliente = response;
       },
       (errorResponse) => {
         this.success = false;
