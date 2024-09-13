e9640e2957bdc25d50995e3143cbe320327e1280
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-13 10:51:42 -0300

    Imports HttpClienteModule in AppModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { TemplateModule } from './template/template.module';
 import { HomeComponent } from './home/home.component';
 import { ClientesModule } from './clientes/clientes.module';
+import { ClientesService } from './clientes.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ClientesModule } from './clientes/clientes.module';
     BrowserModule,
     AppRoutingModule,
     TemplateModule,
-    ClientesModule
+    ClientesModule,
+    HttpClientModule
   ],
   providers: [ClientesService],
   bootstrap: [AppComponent]
