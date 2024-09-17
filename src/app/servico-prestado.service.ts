30341a09358ea82180bf4e1ea26884f4c949aaf0
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-17 15:45:07 -0300

    Adds ServicoPrestadoService for handling API interactions.

---
+++ src/app/servico-prestado.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ServicoPrestado } from './servico-prestado/servicoPrestado';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ServicoPrestadoService {
+  private apiUrl = `${environment.apiUrl}/servicos-prestados`;
+
+  constructor(private http: HttpClient) {}
+
+  salvar(servicoPrestado: ServicoPrestado): Observable<ServicoPrestado> {
+    return this.http.post<ServicoPrestado>(this.apiUrl, servicoPrestado);
+  }
+}
