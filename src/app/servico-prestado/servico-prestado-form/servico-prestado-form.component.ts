4b5d0a62d875030c0f2e249fb2c7c32c4eb9e942
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-17 14:15:08 -0300

    Add ClientesService to ServicoPrestadoFormComponent and initialize clientes array

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientesService } from 'src/app/clientes.service';
+import { Cliente } from 'src/app/clientes/cliente';
 
 @Component({
   selector: 'app-servico-prestado-form',
   templateUrl: './servico-prestado-form.component.html',
-  styleUrls: ['./servico-prestado-form.component.css']
+  styleUrls: ['./servico-prestado-form.component.css'],
 })
 export class ServicoPrestadoFormComponent implements OnInit {
+  clientes: Cliente[] = [];
 
-  constructor() { }
+  constructor(private clienteService: ClientesService) {}
 
   ngOnInit(): void {
   }
