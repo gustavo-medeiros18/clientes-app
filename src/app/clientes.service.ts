7ae6b4e3844e666847084ecb8d82a3b10eb33d95
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-17 15:45:29 -0300

    Refactors ClientesService to use enviroment apiUrl for HTTP requests.

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -2,35 +2,28 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './clientes/cliente';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClientesService {
+  private apiUrl = `${environment.apiUrl}/clientes`;
+
   constructor(private http: HttpClient) {}
-  /**
-   * An Observable is a data type provided by the RxJS library that represents a
-   * stream of data that can be observed over time.
-   *
-   * It is used here to handle asynchronous operations, such as HTTP requests,
-   * allowing the application to react to the data once it is available.
-   */
+
   salvar(cliente: Cliente): Observable<Cliente> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.post<Cliente>(
-      'http://localhost:8080/api/clientes',
-      cliente,
-      { headers }
-    );
+    return this.http.post<Cliente>(this.apiUrl, cliente, {
+      headers,
+    });
   }
 
   atualizar(cliente: Cliente): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.put<Cliente>(
-      `http://localhost:8080/api/clientes/${cliente.id}`,
-      cliente,
-      { headers }
-    );
+    return this.http.put<Cliente>(`${this.apiUrl}/${cliente.id}`, cliente, {
+      headers,
+    });
   }
 
   getCliente(): Cliente {
@@ -42,16 +35,14 @@ export class ClientesService {
   }
 
   getClienteById(id: number): Observable<Cliente> {
-    return this.http.get<any>(`http://localhost:8080/api/clientes/${id}`);
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
   }
 
   getClientes(): Observable<Cliente[]> {
-    return this.http.get<Cliente[]>('http://localhost:8080/api/clientes');
+    return this.http.get<Cliente[]>(this.apiUrl);
   }
 
   deletar(cliente: Cliente): Observable<any> {
-    return this.http.delete<any>(
-      `http://localhost:8080/api/clientes/${cliente.id}`
-    );
+    return this.http.delete<any>(`${this.apiUrl}/${cliente.id}`);
   }
 }
