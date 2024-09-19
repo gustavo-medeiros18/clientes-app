cdafac0099644bfe578428ff10e4e11faf0e1ddf
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-19 15:53:10 -0300

    Implements user registration in LoginComponent with AuthService integration

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-login',
@@ -11,8 +13,9 @@ export class LoginComponent {
   password: string;
   loginError: boolean;
   cadastrando: boolean;
+  mensagemSucesso: string;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   onSubmit() {
     this.router.navigate(['/home']);
@@ -26,5 +29,25 @@ export class LoginComponent {
 
   cancelaCadastro() {
     this.cadastrando = false;
+  }
+
+  cadastrar() {
+    const usuario = new Usuario();
+
+    usuario.username = this.username;
+    usuario.password = this.password;
+
+    this.authService.salvar(usuario).subscribe(
+      (response) => {
+        this.loginError = false;
+        this.mensagemSucesso =
+          'Cadastro realizado com sucesso! Efetue o login.';
+        // this.router.navigate(['/home']);
+      },
+      (errorResponse) => {
+        this.loginError = true;
+        this.mensagemSucesso = null;
+      }
+    );
   }
 }
