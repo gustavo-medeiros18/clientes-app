68e51182a2c32a48b4b3512adf32534e88c2e469
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-19 10:37:57 -0300

    Adds LoginComponent and FormsModule to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,14 @@ import { ClientesModule } from './clientes/clientes.module';
 import { ClientesService } from './clientes.service';
 import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module';
 import { ServicoPrestadoService } from './servico-prestado.service';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     TemplateModule,
     ClientesModule,
