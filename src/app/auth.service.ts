f25ca2ae025132f4b9aae3dd5ec806024296c48a
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-19 15:52:46 -0300

    Adds AuthService for user authentication and API interaction.

---
+++ src/app/auth.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Usuario } from './login/usuario';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  apiUrl: string = `${environment.apiUrl}/usuarios`;
+
+  constructor(private http: HttpClient) {}
+
+  salvar(usuario: Usuario): Observable<any> {
+    return this.http.post<any>(this.apiUrl, usuario);
+  }
+}
