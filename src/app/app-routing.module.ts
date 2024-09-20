0efb1694976acb3cef71b7c68e22f60ce529f578
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-20 15:18:39 -0300

    Add AuthGuard to protect home route from unauthorized access

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,26 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './layout/layout.component';
+import { AuthGuard } from './auth.guard';
 
+/**
+ * Defines the routes for the application.
+ *
+ * - `/login`: Route for the login page, handled by `LoginComponent`.
+ * - `/`: Root route, handled by `LayoutComponent` with child routes:
+ *   - `/home`: Route for the home page, handled by `HomeComponent` and protected by `AuthGuard`.
+ *
+ * @constant
+ * @type {Routes}
+ */
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: '',
     component: LayoutComponent,
-    children: [{ path: 'home', component: HomeComponent }],
+    children: [
+      { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+    ],
   },
 ];
 
