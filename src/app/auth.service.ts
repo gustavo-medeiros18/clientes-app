6c33e05875f895173c20f8a30b74be549f0017b0
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-20 11:55:22 -0300

    Implements OAuth2 login method in AuthService with Basic Authentication

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Usuario } from './login/usuario';
 import { Observable } from 'rxjs';
@@ -17,5 +17,25 @@ export class AuthService {
 
   salvar(usuario: Usuario): Observable<any> {
     return this.http.post<any>(this.apiUrl, usuario);
+  }
+
+  tentarLogar(username: string, password: string): Observable<any> {
+    const params = new HttpParams()
+      .set('username', username)
+      .set('password', password)
+      .set('grant_type', 'password');
+
+    const headers = {
+      /**
+       * The btoa function encodes a string in base-64. In this case, it is used to encode
+       * the clientId and clientSecret in base-64 format to create the Basic Authentication
+       * header. This header is necessary for OAuth2 authentication to securely transmit
+       * the client credentials to the authorization server.
+       */
+      Authorization: 'Basic ' + btoa(`${this.clientId}:${this.clientSecret}`),
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
+
+    return this.http.post(this.tokenUrl, params.toString(), { headers });
   }
 }
