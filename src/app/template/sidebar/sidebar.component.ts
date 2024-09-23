1d001d07f097f311ff6c5fdd717db34c01b7ddf0
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-23 11:53:59 -0300

    Fixes formatting in SidebarComponent styleUrls declaration

--- src/app/template/sidebar/sidebar.component.ts
+++ src/app/template/sidebar/sidebar.component.ts
@@ -5,7 +5,7 @@ import { AuthService } from 'src/app/auth.service';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
   usuarioLogado: string;
