5c7a512bcf13cffaa436207d1c645856f8ad7d44
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-09-23 11:04:20 -0300

    Adds Router dependency to AuthGuard for navigation control

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -3,6 +3,7 @@ import {
   CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
+  Router,
 } from '@angular/router';
 import { AuthService } from './auth.service';
 
@@ -33,7 +34,7 @@ import { AuthService } from './auth.service';
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
